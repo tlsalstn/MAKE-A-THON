8618887140763b516b68dd8ca1deb129eba2c329
ShinMS <39144276+QMCHE@users.noreply.github.com>
2019-10-31 20:59:41 +0900

    Update Main.js

--- saving-house/src/components/Main.js
+++ saving-house/src/components/Main.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './Main.css';
 
 class Main extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="main">
+                <div className="header">
+
+                </div>
+                <div className="footer">
+
+                </div>
             </div>
         );
     };
