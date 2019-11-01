ed821b4af381daef71bd40fa945cf88537f7480a
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-11-02 07:52:40 +0900

    Update ReportList.js

--- saving-house/src/components/ReportList.js
+++ saving-house/src/components/ReportList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Background from '../containers/Background';
 import List from '../containers/List';
+import './ReportList.css';
 
 class ReportList extends Component {
     render() {
         return (
             <div>
                 <Background>
+                    <p className="reportTitle">신고 리스트</p>
                     <List/>
                 </Background>
             </div>
