61c8f534db8b4fa682958df4b07fba781dd34f23
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-11-01 18:54:09 +0900

    Update

--- saving-house/src/stores/KakaoMap.js
+++ saving-house/src/stores/KakaoMap.js
@@ -3,19 +3,30 @@ import axios from 'axios';
 
 configure({enforceActions: "observed"});
 
+const appkey = "7566ec4e8c97a31dd7d4d53420495eda";
+
 export default class KakaoMap {
-    @observable data=[];
+    @observable data = [];
 
-    @action setData = (data) => {this.data = [...data];}
+    @action setData = (datas) => {this.data = [...datas];}
 
-    @action getData() {
-        let result = axios({
-            url: "https://dapi.kakao.com/v2/local/search/address.json",
-            method: "GET",
-            params: {
-                query: "전북 삼성동 100"
-            }
-        });
-        this.setData(result);
+    @action async getData() {
+        try{
+            let result = await axios({
+                url: "https://dapi.kakao.com/v2/local/search/address.json",
+                method: "GET",
+                headers: {
+                    "Authorization": `KakaoAK ${appkey}`
+                },
+                params: {
+                    query: "전북 삼성동 100"
+                }
+            });
+            console.log(result.data.documents);
+            this.setData(result);
+            console.log(this.data);
+        } catch(err) {
+            console.log(err);
+        }
     };
 };
