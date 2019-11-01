de7e51d718860cb82137a5261a7d4579fcf8abd7
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-11-02 07:52:52 +0900

    Update DispatchBtn.js

--- saving-house/src/containers/DispatchBtn.js
+++ saving-house/src/containers/DispatchBtn.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './DispatchBtn.css';
 import { inject, observer } from 'mobx-react';
-
-// const DispatchBtn = ({ id, status }) => (
-//     <div className="dispatchBtn">
-//         {status === 0 ?
-//             <button onClick={() => dispatchBtn(id)} className="enable">출동</button> : <button className="disable">출동완료</button>
-//         }
-//     </div>
-// );
 
 @inject("report")
 @observer
