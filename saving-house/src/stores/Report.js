9a5440a8f5aac43525210766e98b242b1ea5b7c6
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-11-02 07:53:21 +0900

    Update Report.js

--- saving-house/src/stores/Report.js
+++ saving-house/src/stores/Report.js
@@ -3,21 +3,24 @@ import axios from 'axios';
 
 configure({enforceActions: "observed"});
 
+const serverUri = "http://172.16.10.9:3002";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFhYSIsImlhdCI6MTU3MjYwOTMzMCwiZXhwIjoxNTc3NzkzMzMwLCJpc3MiOiJER1NXLkhTLktSIiwic3ViIjoidG9rZW4ifQ.x94hZza_gRizzxpgr9jSuMziIS133XVkwVWMhTRwhxE";
 
 export default class Report {
     @observable report = [];
+    @observable reportInfo = {};
 
-    @action setReport = (reports) => {this.report = [...reports]}
+    @action setReport = (reports) => {this.report = [...reports];}
+    @action setReportInfo = (reportInfo) => {this.reportInfo = {...reportInfo};}
 
     @action async getReport() {
         try{
             let result = await axios({
-                url: "http://172.16.10.9:3002/api/report",
+                url: serverUri + "/api/report",
                 method: "GET",
                 headers: {
                     "Access-Control-Allow-Origin": true,
-                    "token": `${token}`,
+                    "token": token,
                 },
             });
             this.setReport(result.data.data);
@@ -26,14 +29,30 @@ export default class Report {
         }
     }
 
-    @action async changeState(id) {
+    @action async getReportInfo(id) {
         try{
             let result = await axios({
-                url: "http://172.16.10.9:3002/api/report/changeState/" + id,
+                url: serverUri + "/api/report/reportInfo/" + id,
+                method: "GET",
+                headers: {
+                    "Access-Control-Allow-Origin": true,
+                    "token": token,
+                },
+            });
+            this.setReportInfo(result.data);
+        }catch(error) {
+            console.log(error);
+        }
+    }
+
+    @action async changeState(id) {
+        try{
+            await axios({
+                url: serverUri + "/api/report/changeState/" + id,
                 method: "PATCH",
                 headers: {
                     "Access-Control-Allow-Origin": true,
-                    "token": `${token}`,
+                    "token": token,
                 },
                 data: {
                     "rescueState": 1,
